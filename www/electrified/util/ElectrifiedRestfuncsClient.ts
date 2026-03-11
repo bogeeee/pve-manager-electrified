521b17c79bf0c89bc787d32a3f5690fb1f5e74cb
Boris Gingold <bogeee@pve-electrified.org>
2026-03-11 20:36:25 +0100

    Bugfix: ElectrifiedAPI requests may fail in a ticket-renewal situation which is done every 15 minutes

--- www/electrified/util/ElectrifiedRestfuncsClient.ts
+++ www/electrified/util/ElectrifiedRestfuncsClient.ts
@@ -1,5 +1,6 @@
 import {RestfuncsClient} from "restfuncs-client";
 import type {IServerSession} from "restfuncs-common";
+import {RetryableError, retryTilSuccess} from "./util";
 
 interface WithPermissionCheck extends IServerSession {
     ensurePermissionsAreUp2Date(): Promise<void>;
@@ -10,16 +11,20 @@ interface WithPermissionCheck extends IServerSession {
  */
 export class ElectrifiedRestfuncsClient<S extends WithPermissionCheck> extends RestfuncsClient<S> {
     async doCall(methodName: string, args: any[]) {
-        try {
-            return await super.doCall(methodName, args);
-        }
-        catch (e) {
-            if(e?.cause?.name === "NeedToRefreshPermissionsViaHttp") {
-                await this.doCall_http("ensurePermissionsAreUp2Date",[]);
+        return retryTilSuccess(async () => { // Exec with quiet some possible retries after a NeedToRefreshPermissionsViaHttp, see below.
+            try {
                 return await super.doCall(methodName, args);
             }
+            catch (e) {
+                if(e?.cause?.name === "NeedToRefreshPermissionsViaHttp") {
+                    await this.doCall_http("ensurePermissionsAreUp2Date",[]);
+                    // return await super.doCall(methodName, args); // Possible race condition when ticket is renewed just before this line. So instead we retry a few times.
+                    throw new RetryableError();
+                }
 
-            throw e;
-        }
+                throw e;
+            }
+        }, {initialRetryDelay: 0.3, maxTime: 10000 /* observed errors with only 1000ms maxTime (probably pve is stalling unauthorized requests a few seconds in a ticket renewal situation.*/})
+
     }
 }
